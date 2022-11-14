2b0d9848773dc92b0a24144459c37e3ee6554611
Matt <mfouquier1@gmail.com>
2022-11-14 12:59:55 -0500

    refactor to only use 5 lines

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 import { createServer } from 'node:http';
-const server = createServer((req, res) => {
-  res.end('Hello Galvanize!');
-})
-server.listen(3001, 'localhost', () => {
-  const port = 3001;
-  const hostname = 'localhost';
-  console.log(`Server running on ${hostname}:${port}`)
-})
+const port = 3001;
+const hostname = 'localhost';
+const server = createServer((req, res) => res.end('Hello Galvanize!'))
+server.listen(port, () => console.log(`Server running on ${hostname}:${port}`))
